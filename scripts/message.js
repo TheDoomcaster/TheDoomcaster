b3f69d946c0b0d0842ab35ffa0504b253bf5aaf9
Luke (Doomcaster) <LanarkyFaust@gmail.com>
2019-12-03 22:46:44 -0800

    Test
    First Test

---
+++ scripts/message.js
@@ -0,0 +1,9 @@
+
+xxxx = function() {
+var div = document.createElement("div");
+
+div.style.cssText= = "display: block; width: 100px; height: 100px; backfground: #0000bb; color: #ffffff;";
+div.innerHTML = "Hello";
+
+document.body.appendChild(div);
+}
