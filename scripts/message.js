8478c0da3f5d55e5613df645dc04bda664f4c74f
Luke (Doomcaster) <LanarkyFaust@gmail.com>
2019-12-04 17:55:32 -0800

    Removed unused folder

--- scripts/message.js
+++
@@ -1,9 +0,0 @@
-
-xxxx = function() {
-var div = document.createElement("div");
-
-div.style.cssText= = "display: block; width: 100px; height: 100px; backfground: #0000bb; color: #ffffff;";
-div.innerHTML = "Hello";
-
-document.body.appendChild(div);
-}
